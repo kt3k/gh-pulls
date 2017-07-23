93d264f452b54a2db425ce9ed09aa6a122395082
Yoshiya Hinosawa <stibium121@gmail.com>
2017-07-23 16:50:09 +0900

    test: fix PATH for cirlce ci testing

--- test.js
+++ test.js
@@ -3,12 +3,14 @@ const { expect } = require('chai')
 const { exec } = require('child_process')
 
 const API_ROOT = 'http://localhost:28987'
+const PATH = process.env.PATH
+const env = { API_ROOT, PATH }
 
 describe('gh-pulls', () => {
   timeout(10000)
 
   it('fetches pull request count', done => {
-    exec('node index.js mojombo', { env: { API_ROOT } }, (err, stdout) => {
+    exec('node index.js mojombo', { env }, (err, stdout) => {
       expect(err).to.equal(null)
       expect(stdout.toString().trim()).to.equal('mojombo: 43')
       done()
@@ -16,7 +18,7 @@ describe('gh-pulls', () => {
   })
 
   it('throws error when the parameter is not given', done => {
-    exec('node index.js', { env: { API_ROOT } }, (err) => {
+    exec('node index.js', { env }, (err) => {
       expect(err).to.not.equal(null)
       done()
     })
